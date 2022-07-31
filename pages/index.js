32e076ce0b56c6ae0f25b3d1df5293d7a39f1914
johnmuthua <caveomuthama@gmail.com>
2022-07-31 20:45:46 +0300

    feat-Added footer

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -6,26 +7,43 @@ export default function Home() {
       {/* Header */}
       <Head>
         {/* logo */}
-        <div className="flex flex-row justify-between bg-black p-0 m-0">
+        <div className="flex flex-row justify-between bg-black pl-2 m-0">
           <div className="flex flex-col text-white">
             <div className=" text-2xl">PATO</div>
             <div className=" text-sm">International</div>
             <div className="text-xs">+253 444 333</div>
           </div>
           {/* Mid */}
-          <div className="">
-            <div>TICKETS</div>
-            <div>PREVIOUS LINEUPS</div>
-            <div>MERCH</div>
-            <div>SUSTAINABILITY</div>
-            <div>MEDIA</div>
-            <div>HELP</div>
-            <div>MORE</div>
-            <div>WORLDWIDE</div>
+          {/* Todo: Make it hidden in small devices */}
+          <div className="text-gray-400 font-xs xs:hidden md:visible flex flex-row justify-between lg:visible mt-8">
+            <div className="pr-2">
+              <Link href="/">TICKETS</Link>
+            </div>
+            <div className="pr-2">
+              <Link href="/">PREVIOUS LINEUPS</Link>
+            </div>
+            <div className="pr-2">
+              <Link href="/">MERCH</Link>
+            </div>
+            <div className="pr-2">
+              <Link href="/">SUSTAINABILITY</Link>
+            </div>
+            <div className="pr-2">
+              <Link href="/">MEDIA</Link>
+            </div>
+            <div className="pr-2">
+              <Link href="/">HELP</Link>
+            </div>
+            <div className="pr-2">
+              <Link href="/">MORE</Link>
+            </div>
+            <div className="pr-2">
+              <Link href="/">WORLDWIDE</Link>
+            </div>
           </div>
           {/* login */}
-          <div className="flex flex-row text-white">
-            <div className="flex flex-row text-white xs:hidden pr-2">
+          <div className="flex flex-row  mt-8 font-sm ">
+            <div className="flex flex-row xs:hidden pr-2 mr-4 text-gray-400">
               <div className="">Create My Account / </div>
               <div className="">Login</div>
             </div>
@@ -36,6 +54,14 @@ export default function Home() {
           </div>
         </div>
       </Head>
+      <footer>
+        <div className="">
+          {/* Call to action button  */}
+          <div className=""></div>
+          <div className=""></div>
+          <div className=""></div>
+        </div>
+      </footer>
     </div>
   );
 }
