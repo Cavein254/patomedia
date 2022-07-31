f845d23512c16336cbe138163a1a7bc03a5a6463
johnmuthua <caveomuthama@gmail.com>
2022-07-31 21:33:33 +0300

    feat-Added footer links

--- pages/index.js
+++ pages/index.js
@@ -54,10 +54,74 @@ export default function Home() {
           </div>
         </div>
       </Head>
-      <footer>
-        <div className="">
+      <footer className="">
+        <div className="bg-black">
           {/* Call to action button  */}
-          <div className=""></div>
+          <div className=" flex justify-between flex-col-reverse md:flex-row-reverse px-24 mx-auto">
+            <form className="pb-12">
+              <div>
+                {/* form image  */}
+                <div className="flex flex-col min-w-full justify-center items-center">
+                  <div>Image</div>
+                  <div className="text-white pt-4">
+                    Do you have an account? Sign up to register for events, earn
+                    points, and more!
+                  </div>
+                  <div className="pt-4">
+                    <input
+                      className="text-lg text-white px-2 py-4 rounded-sm bg-black ring-white border-x-white ring-opacity-50 focus:ring-blue-300 focus:border-2 focus:ring-2 focus:border-blue-300"
+                      type="text"
+                      placeholder="Enter email address to get started..."
+                    />
+                  </div>
+                  <div className="pt-4">
+                    {/* Todo: Add a forward button ">" */}
+                    <button
+                      type="submit"
+                      className="text-black bg-blue-300 text-lg max-w-full rounded-sm px-4 py-2 "
+                    >
+                      GET STARTED{" "}
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </form>
+            {/* Links */}
+            <div className="text-white flex flex-row justify-around">
+              <div className="">
+                <div className="">LineUp</div>
+                <div className="">Tickets</div>
+                <div className="">Previous LineUps</div>
+                <div className="">LiveSets</div>
+                <div className="">Gallery</div>
+                <div className="">News</div>
+                <div className="">Help</div>
+                <div className="">Contact Us</div>
+              </div>
+              <div className="">
+                <div className="">Ultra Worldwide</div>
+                <div className="">Road to Ultra</div>
+                <div className="">RESISTANCE</div>
+                <div className="">UMF Films</div>
+                <div className="">UMF Tv</div>
+                <div className="">UMF Radio</div>
+              </div>
+            </div>
+            {/* Large logo */}
+            <div className="">
+              <div className="text-white">
+                <div className=" text-2xl">PATO</div>
+                <div className=" text-sm">International</div>
+                <div className="text-xs">+253 444 333</div>
+                <div className="text-xs">Kasarani Nairobi</div>
+                <div className="text-xl pt-8">240 days, 03:33:11</div>
+              </div>
+              <div className=""></div>
+              <div className=""></div>
+              <div className=""></div>
+            </div>
+          </div>
+          <hr className="border-2 border-blue-300" />
           <div className=""></div>
           <div className=""></div>
         </div>
